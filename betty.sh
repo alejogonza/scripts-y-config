2aec1ab2688e59cd9bf9038b1faba8a078b0fa69
alejogonza <alejo.1996.2001@gmail.com>
2019-07-04 14:58:57 +0000

    script

---
+++ betty.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+git clone https://github.com/holbertonschool/Betty.git
+cd Betty/
+sudo ./install.sh
+echo "#!/bin/bash
+# Simply a wrapper script to keep you from having to use betty-style
+# and betty-doc separately on every item.
+# Originally by Tim Britton (@wintermanc3r), multiargument added by
+# Larry Madeo (@hillmonkey)
+BIN_PATH="/usr/local/bin"
+BETTY_STYLE="betty-style"
+BETTY_DOC="betty-doc"
+if [ "$#" = "0" ]; then
+    echo "No arguments passed."
+    exit 1
+fi
+for argument in "$@" ; do
+    echo -e "\n========== $argument =========="
+    ${BIN_PATH}/${BETTY_STYLE} "$argument"
+    ${BIN_PATH}/${BETTY_DOC} "$argument"
+done" > betty
+chmod a+x betty
+sudo mv betty /bin/
---
+++ files1
@@ -0,0 +1,4 @@
+touch $FILE && chmod u+x $FILE && echo '#!/bin/bash' >> $FILE && emacs $FILE
+
+
+#export FILE= namefile
